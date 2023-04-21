e5559db8ae00fb88435aaef24ce29cb34827477b
Keating <min9nim@gmail.com>
2023-04-22 00:23:57 +0900

    fix ts error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import {Inter} from 'next/font/google'
 
 const inter = Inter({subsets: ['latin']})
 
-export default function Home({tx, data}) {
+interface IProps {
+  tx: string | null
+  data: string| null
+}
+export default function Home({tx, data}: IProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
