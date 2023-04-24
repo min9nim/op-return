c083807a36bbc38c873d4e315d46bbe6507d297d
Keating <min9nim@gmail.com>
2023-04-24 09:40:52 +0900

    🎨 Add or update UI/UX

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,26 @@ const inter = Inter({subsets: ['latin']})
 
 interface IProps {
   tx: string | null
-  data: string| null
+  data: string | null
 }
+
+function TxLink({txid}) {
+  return <a
+    href={'https://mempool.space/tx/' + txid}
+    className="group rounded-lg border border-transparent px-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{fontStyle: 'italic'}}
+  >{txid}</a>
+}
+
 export default function Home({tx, data}: IProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {tx ? `on ${tx}` : 'Engrave your message on eternity of Bitcoin.'}
+          {tx ? <div style={{display: 'flex', alignItems: 'center'}}>Message on<TxLink txid={tx}/></div> : 'Engrave your message on eternity of Bitcoin.'}
         </p>
         <div
           className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -115,7 +126,7 @@ export default function Home({tx, data}: IProps) {
 
 // This gets called on every request
 export async function getServerSideProps(req, res, resolvedUrl) {
-  const tx =  req.query.tx
+  const tx = req.query.tx
 
   if (!tx) {
     return {
